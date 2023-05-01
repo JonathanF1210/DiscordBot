ef1358816b2f96e6310082bf66df1d3b546453b0
JonathanF1210 <jonathanf1210@gmail.com>
2023-05-01 13:24:45 -0500

    added bot response when a member joins server. Also enabled caching and added method that will make the bot respond when a the online status of member is update. This message will show how many members in the server are currenly online

--- src/main/java/my/group/listeners/EventListener.java
+++ src/main/java/my/group/listeners/EventListener.java
@@ -1,11 +1,17 @@
 package my.group.listeners;
 
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.events.user.update.UserUpdateOnlineStatusEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class EventListener extends ListenerAdapter {
 
@@ -23,11 +29,33 @@ public class EventListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
        String message = event.getMessage().getContentRaw();
-        MessageChannel channel = event.getChannel();
+       MessageChannel channel = event.getChannel();
        if (message.toLowerCase().contains("ping")){
            channel.sendMessage("Pong!").queue();
        } else if (message.toLowerCase().replaceAll(" ", "").contains("hellofirstbot")){
            channel.sendMessage("Hello " + event.getMember().getEffectiveName()).queue();
        }
+    }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        event.getGuild().getDefaultChannel().asStandardGuildMessageChannel().sendMessage( event.getMember().getEffectiveName() + " has joined the server. Welcome them and have fun!").queue();
+    }
+
+    @Override
+    public void onUserUpdateOnlineStatus(UserUpdateOnlineStatusEvent event) {
+        List<Member> members = event.getGuild().getMembers();
+        int onlineMembers = 0;
+        for (Member member : members){
+            if(member.getOnlineStatus() == OnlineStatus.ONLINE){
+                onlineMembers++;
+            }
+        }
+
+
+        User user = event.getUser();
+        String message = "**" + user.getAsTag() + "** updated their status to " + event.getNewOnlineStatus().getKey() + "!" +
+                        "\nThere are now " + onlineMembers + " user(s) online!";
+        event.getGuild().getDefaultChannel().asStandardGuildMessageChannel().sendMessage(message).queue();
     }
 }
