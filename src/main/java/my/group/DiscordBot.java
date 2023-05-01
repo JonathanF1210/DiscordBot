70cff42f4bb2e12e7cc33217d80158e0f44b4c73
JonathanF1210 <jonathanf1210@gmail.com>
2023-05-01 12:03:56 -0500

    Event listner for reactions to messages and a ping ping response, also added GatewayIntents into shardManager builder

--- src/main/java/my/group/DiscordBot.java
+++ src/main/java/my/group/DiscordBot.java
@@ -1,26 +1,35 @@
 package my.group;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import my.group.listeners.EventListener;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DiscordBot {
     private final Dotenv config;
     private final ShardManager shardManager;
+    private final GatewayIntent[] gatewayIntents;
 
     public DiscordBot() throws LoginException{
         config = Dotenv.configure().load();
         String token = config.get("TOKEN");
+        gatewayIntents = new GatewayIntent[]{GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.SCHEDULED_EVENTS};
 
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token, Arrays.asList(gatewayIntents));
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.playing("Valorant-Top Fragging with classic only"));
         builder.build();
         shardManager = builder.build();
+
+        // Register listeners
+        shardManager.addEventListener(new EventListener());
     }
 
     public Dotenv getConfig(){
